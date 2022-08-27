5c28878684200e5a58c2c5438da6ef4ed1248f68
neltia <dsz08082@naver.com>
2022-08-28 02:00:16 +0900

    코드 갱신

--- basic_example/05_naver_login.py
+++ basic_example/05_naver_login.py
@@ -7,8 +7,8 @@ Description: 네이버(naver.com) 자동 로그인(auto login)
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium import webdriver
 import pyperclip
 import time
 
@@ -23,23 +23,29 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get('https://naver.com')
 
 # 2. 로그인 버튼 클릭
-elem = driver.find_element_by_class_name('link_login')
+elem = driver.find_element(By.CLASS_NAME, 'link_login')
 elem.click()
 
+# 3. id, pw 입력
+'''
+driver.find_element(By.ID, 'id').send_keys(user_id)
+driver.find_element(By.ID, 'pw').send_keys(user_pw)
+'''
+
 # 3. id 복사 붙여넣기
-elem_id = driver.find_element_by_id('id')
+elem_id = driver.find_element(By.ID, 'id')
 elem_id.click()
 pyperclip.copy(user_id)
 elem_id.send_keys(Keys.CONTROL, 'v')
 time.sleep(1)
 
 # 4. pw 복사 붙여넣기
-elem_pw = driver.find_element_by_id('pw')
+elem_pw = driver.find_element(By.ID, 'pw')
 elem_pw.click()
 pyperclip.copy(user_pw)
 elem_pw.send_keys(Keys.CONTROL, 'v')
 time.sleep(1)
 
 # 5. 로그인 버튼 클릭
-driver.find_element_by_id('log.login').click()
-time.sleep(2)
+driver.find_element(By.ID, 'log.login').click()
+time.sleep(3)
